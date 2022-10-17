cf69ba9039c5e6da8538e97e05abbe5face4e524
Lucas Santos <lucas.silva1203@outlook.com>
2022-10-17 20:18:19 -0300

    Add function that closes popups

--- script/script.js
+++ script/script.js
@@ -1,10 +1,16 @@
 let addBookContainer = document.querySelector("#card-creator-container")
 
+let closeButtons = document.querySelectorAll(".cancel")
 let addBookPopup = document.querySelector("#card-creator-popup")
-addBookPopup.addEventListener("click", () =>{
+function closePopup(){
     addBookPopup.style.display = "none"
     addBookContainer.style.display = "none"
+}
+addBookPopup.addEventListener("click", closePopup)
+closeButtons.forEach((button) => {
+    button.addEventListener("click", closePopup)
 })
+
 
 let addBookButton = document.querySelector("#add-book-btn")
 addBookButton.addEventListener("click", () =>{
@@ -21,14 +27,24 @@ function getStatus(){
     statusPanel.textContent = `Total: ${books.length} Read: Not Read: Dropped:`
 }
 
+// let addSymbol = document.createElement("div")
+// addSymbol.classList.add("add-symbol")
+
+let addBookCover = document.createElement("div")
+// addBookCover.appendChild(addSymbol)
+addBookCover.id = "add-book-cover"
+addBookCover.textContent = "+"
+
 function checker(){
     if(books.length == 0){
+        bookshelf.textContent = ""
         bookshelf.style.display = "flex"
         bookshelf.classList.add("empty-bookshelf")
         bookshelf.textContent = "Seems like you haven't added anything yet."
     } else {
         bookshelf.style.display = "grid"
         bookshelf.classList.remove("empty-bookshelf")
+        bookshelf.appendChild(addBookCover)
     }
     getStatus()
 }
