1ce5922c1078ab9a4b26fa8717990f4e543b0a2d
Lucas Santos <lucas.silva1203@outlook.com>
2022-10-17 18:47:11 -0300

    Add function that checks for any book

--- script/script.js
+++ script/script.js
@@ -1,11 +1,48 @@
-function Book(title, author, pages, read){
+let addBookContainer = document.querySelector("#card-creator-container")
+
+let addBookPopup = document.querySelector("#card-creator-popup")
+addBookPopup.addEventListener("click", () =>{
+    addBookPopup.style.display = "none"
+    addBookContainer.style.display = "none"
+})
+
+let addBookButton = document.querySelector("#add-book-btn")
+addBookButton.addEventListener("click", () =>{
+    addBookPopup.style.display = "block"
+    addBookContainer.style.display = "flex"
+    console.log("oi")
+})
+
+let bookshelf = document.querySelector("#bookshelf")
+let books = document.querySelectorAll(".book")
+
+function checker(){
+    if(books.length == 0){
+        bookshelf.style.display = "flex"
+        bookshelf.classList.add("empty-bookshelf")
+        bookshelf.textContent = "Seems like you haven't added anything yet."
+    } else {
+        bookshelf.style.display = "grid"
+        bookshelf.classList.remove("empty-bookshelf")
+    }
+}
+checker()
+
+
+
+
+
+
+
+
+
+function Book(title, author, pages, read, dateAdded, dateUpdated=dateAdded){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = () => {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
+    this.dateAdded = dateAdded;
+    this.dateUpdated = dateUpdated;
 }
 
 let hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
@@ -14,4 +51,3 @@ Book.prototype.oi = () => {
 }
 
 console.log(hobbit.oi())
-
