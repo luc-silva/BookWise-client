4ee0ece697aba4e3ee06b06a380090294a7aa21d
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-18 18:11:15 -0300

    Add default values for inputs, forms and states

---
+++ src/constants/defaultValues.ts
@@ -0,0 +1,18 @@
+export const createBookFormDefaultValues: BookDetails = {
+    edition: "",
+    franchise: "",
+    description: "",
+    title: "",
+    author: "",
+    tags: "",
+    pages: 0,
+    release_date: new Date(),
+    store_url: "",
+    volume: "",
+};
+
+export const userSessionDefaultValues = {
+    username: "",
+    token: "",
+    isLogged: false,
+};
